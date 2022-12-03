9b30d6ab3a157bff4b081afa3f4df107e6b93695
QuadnucYard <2380433991@qq.com>
2022-12-03 19:06:58 +0800

    lab3 - arrange

--- kern/trap.c
+++ kern/trap.c
@@ -92,17 +92,20 @@ trap_init(void)
 	SETGATE(idt[T_BOUND], 0, GD_KT, handler_bound, 0);
 	SETGATE(idt[T_ILLOP], 0, GD_KT, handler_illop, 0);
 	SETGATE(idt[T_DEVICE], 0, GD_KT, handler_device, 0);
-	SETGATE(idt[T_SIMDERR], 0, GD_KT, handler_simderr, 0);
-	SETGATE(idt[T_FPERR], 0, GD_KT, handler_fperr, 0);
-	SETGATE(idt[T_MCHK], 0, GD_KT, handler_mchk, 0);
-	SETGATE(idt[T_SYSCALL], 0, GD_KT, handler_syscall, 3);
 	SETGATE(idt[T_DBLFLT], 0, GD_KT, handler_dblflt, 0);
+
 	SETGATE(idt[T_TSS], 0, GD_KT, handler_tss, 0);
 	SETGATE(idt[T_SEGNP], 0, GD_KT, handler_segnp, 0);
 	SETGATE(idt[T_STACK], 0, GD_KT, handler_stack, 0);
 	SETGATE(idt[T_GPFLT], 0, GD_KT, handler_gpflt, 0);
 	SETGATE(idt[T_PGFLT], 0, GD_KT, handler_pgflt, 0);
+
+	SETGATE(idt[T_SIMDERR], 0, GD_KT, handler_simderr, 0);
+	SETGATE(idt[T_FPERR], 0, GD_KT, handler_fperr, 0);
 	SETGATE(idt[T_ALIGN], 0, GD_KT, handler_align, 0);
+	SETGATE(idt[T_MCHK], 0, GD_KT, handler_mchk, 0);
+
+	SETGATE(idt[T_SYSCALL], 0, GD_KT, handler_syscall, 3);
 
 	// Per-CPU setup 
 	trap_init_percpu();
