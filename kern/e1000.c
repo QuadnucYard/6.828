8d82befa5a9ed1af019755bf09184c86fe7cf71c
QuadnucYard <2380433991@qq.com>
2022-12-07 16:39:54 +0800

    lab6 - ex4

--- kern/e1000.c
+++ kern/e1000.c
@@ -3,8 +3,13 @@
 
 // LAB 6: Your driver code here
 
+volatile uint32_t* e1000;
+
 int
 pci_e1000_attach(struct pci_func* pcif) {
 	pci_func_enable(pcif);
+	cprintf("attach e1000: %x %x\n", pcif->reg_base[0], pcif->reg_size[0]);
+	e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+	cprintf("Register Set: %p, %p, %p, ...\n", e1000[0], e1000[1], e1000[2]);
 	return 1;
 }
