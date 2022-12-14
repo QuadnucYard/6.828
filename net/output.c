47ac1cfe2cfb1b38fb9514fd65a65677dc513506
QuadnucYard <2380433991@qq.com>
2022-12-14 11:26:19 +0800

    lab6 - ex8

--- net/output.c
+++ net/output.c
@@ -10,4 +10,12 @@ output(envid_t ns_envid)
 	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
+	int perm;
+	envid_t eid;
+	while (true) {
+		while (ipc_recv(&eid, &nsipcbuf, &perm) != NSREQ_OUTPUT)
+			sys_yield();
+		while (sys_net_try_send(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len) < 0)
+			sys_yield();
+	}
 }
