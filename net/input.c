078bf04af466b7e46416e0f9e16d72ad679a6acb
QuadnucYard <2380433991@qq.com>
2022-12-14 21:36:15 +0800

    lab6 - ex12

--- net/input.c
+++ net/input.c
@@ -1,6 +1,10 @@
 #include "ns.h"
 
+#define PKT_COUNT 3
+
 extern union Nsipc nsipcbuf;
+
+static struct jif_pkt* pkt = (struct jif_pkt*)REQVA;
 
 void
 input(envid_t ns_envid)
@@ -13,4 +17,21 @@ input(envid_t ns_envid)
 	// Hint: When you IPC a page to the network server, it will be
 	// reading from it for a while, so don't immediately receive
 	// another packet in to the same physical page.
+
+	int r;
+
+	for (int i = 0; i < PKT_COUNT; i++)
+		if ((r = sys_page_alloc(0, (void*)pkt + i * PGSIZE, PTE_P | PTE_U | PTE_W)) < 0)
+			panic("sys_page_alloc: %e", r);
+
+	struct jif_pkt* cpkt = pkt;
+	for (int i = 0; ; i = (i + 1) % PKT_COUNT) {
+		cpkt = (struct jif_pkt*)((void*)pkt + i * PGSIZE);
+		while ((r = sys_net_recv((void*)cpkt + sizeof(cpkt->jp_len), PGSIZE - sizeof(cpkt->jp_len))) < 0)
+			sys_yield();
+
+		cpkt->jp_len = r;
+		ipc_send(ns_envid, NSREQ_INPUT, cpkt, PTE_P | PTE_U | PTE_W);
+		sys_yield();
+	}
 }
