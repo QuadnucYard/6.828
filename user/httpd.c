919cf53f285acaf73b213b2d3bbf8876bbce8f66
QuadnucYard <2380433991@qq.com>
2022-12-15 00:12:49 +0800

    lab6 - ex13+

--- user/httpd.c
+++ user/httpd.c
@@ -109,8 +109,92 @@ send_size(struct http_request *req, off_t size)
 static const char*
 mime_type(const char *file)
 {
-	//TODO: for now only a single mime type
-	return "text/html";
+	// Get file name
+	const char* p = file;
+	for (const char* i = file; *i; i++)
+		if (*i == '/')
+			p = i;
+
+	// Get extension
+	const char* ext = NULL;
+	for (const char* i = p; *i; i++)
+		if (*i == '.')
+			ext = i;
+	if (!ext)
+		return "text/plain";
+
+	const char* mime_map[][2] = {
+		// document
+		{".ai", "application/postscript"},
+		{".eps", "application/postscript"},
+		{".exe", "application/octet-stream"},
+		{".doc", "application/vnd.ms-word"},
+		{".xls", "application/vnd.ms-excel"},
+		{".ppt", "application/vnd.ms-powerpoint"},
+		{".pps", "application/vnd.ms-powerpoint"},
+		{".pdf", "application/pdf"},
+		{".xml", "application/xml"},
+		{".odt", "application/vnd.oasis.opendocument.text"},
+		{".swf", "application/x-shockwave-flash"},
+		// compressed
+		{".gz", "application/x-gzip"},
+		{".tgz", "application/x-gzip"},
+		{".bz", "application/x-bzip2"},
+		{".bz2", "application/x-bzip2"},
+		{".tbz", "application/x-bzip2"},
+		{".zip", "application/zip"},
+		{".rar", "application/x-rar"},
+		{".tar", "application/x-tar"},
+		{".7z", "application/x-7z-compressed"},
+		// text
+		{".txt", "text/plain"},
+		{".php", "text/x-php"},
+		{".html", "text/html"},
+		{".htm", "text/html"},
+		{".js", "text/javascript"},
+		{".css", "text/css"},
+		{".rtf", "text/rtf"},
+		{".rtfd", "text/rtfd"},
+		{".py", "text/x-python"},
+		{".java", "text/x-java-source"},
+		{".rb", "text/x-ruby"},
+		{".sh", "text/x-shellscript"},
+		{".pl", "text/x-perl"},
+		{".sql", "text/x-sql"},
+		// image
+		{".bmp", "image/x-ms-bmp"},
+		{".jpg", "image/jpeg"},
+		{".jpeg", "image/jpeg"},
+		{".gif", "image/gif"},
+		{".png", "image/png"},
+		{".tif", "image/tiff"},
+		{".tiff", "image/tiff"},
+		{".tga", "image/x-targa"},
+		{".psd", "image/vnd.adobe.photoshop"},
+		// audio
+		{".mp3", "audio/mpeg"},
+		{".mid", "audio/midi"},
+		{".ogg", "audio/ogg"},
+		{".mp4a", "audio/mp4"},
+		{".wav", "audio/wav"},
+		{".wma", "audio/x-ms-wma"},
+		// video
+		{".avi", "video/x-msvideo"},
+		{".dv", "video/x-dv"},
+		{".mp4", "video/mp4"},
+		{".mpeg", "video/mpeg"},
+		{".mpg", "video/mpeg"},
+		{".mov", "video/quicktime"},
+		{".wm", "video/x-ms-wmv"},
+		{".flv", "video/x-flv"},
+		{".mkv", "video/x-matroska"},
+	};
+
+	for (int i = 0; i < ARRAY_SIZE(mime_map); i++)
+		if (strcmp(ext, mime_map[i][0]) == 0)
+			return mime_map[i][1];
+
+	return "text/plain";
 }
 
 static int
